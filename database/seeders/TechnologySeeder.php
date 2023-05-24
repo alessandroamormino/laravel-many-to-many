1a180e02f3cbe16df8ff67770c369e5dee3ee601
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-24 13:47:59 +0200

    feat: populate seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +16,21 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        // creo un array di tecnologie per popolare dei dati di test
+        $techs = ['HTML', 'CSS', 'JS', 'PHP', 'Vue', 'Bootstrap', 'Sass', 'MySQL', 'Laravel', 'VS Code', 'Figma'];
+        // creo un record per ogni tecnologia
+        foreach($techs as $tech){
+            // creo un nuovo record
+            $newTech = new Technology();
+            // valorizzo i campi
+            $newTech->name = $tech;
+            $newTech->color = $faker->hexColor();
+            $newTech->slug = Str::slug($newTech->name, '-');
+            // salvo il record
+            $newTech->save();
+        }
+
     }
 }
