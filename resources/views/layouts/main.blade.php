321058c35feff7f6e2072cb02b470b002218c5d7
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-27 09:57:34 +0200

    style: add icon website

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,6 +25,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;400;600&display=swap" rel="stylesheet">
+
+    <link rel="icon" href="{{Vite::asset('resources/img/icon.png')}}">
 </head>
 
 <body>
