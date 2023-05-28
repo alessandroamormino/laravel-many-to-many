281c927c8960192258a361003cde33e54c3c0537
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-28 11:56:55 +0200

    style: add svg icon to explorer

--- resources/views/partials/explorer.blade.php
+++ resources/views/partials/explorer.blade.php
@@ -1,18 +1,25 @@
 <div class="explorer">
   <h6>EXPLORER</h6>
   <div>
-    <span>Portfolio</span>
+    <div class="drop">
+      <i class="fa-solid fa-angle-down"></i>
+      <span>Portfolio</span>
+    </div>
     <div class="files">
-      <div class="dashboard">
+      <div class="dashboard flex">
+        <img src="{{Vite::asset('resources/img/json_icon.svg')}}" alt="" class="explorer-img">
         <a href="{{route('admin.dashboard.home')}}">dashboard</a>
       </div>
-      <div class="projects">
+      <div class="projects flex">
+        <img src="{{Vite::asset('resources/img/json_icon.svg')}}" alt="" class="explorer-img">
         <a href="{{route('admin.projects.index')}}">projects</a>
       </div>
-      <div class="types">
+      <div class="types flex">
+        <img src="{{Vite::asset('resources/img/json_icon.svg')}}" alt="" class="explorer-img">
         <a href="{{route('admin.types.index')}}">types</a>
       </div>
-      <div class="technologies">
+      <div class="technologies flex">
+        <img src="{{Vite::asset('resources/img/json_icon.svg')}}" alt="" class="explorer-img">
         <a href="{{route('admin.technologies.index')}}">technologies</a>
       </div>
     </div>
